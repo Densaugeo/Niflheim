8fd6e40ae65ad5b16643ecd27260dabd9ca0a383
Lunariel <lunariel@morpheus.lan>
2015-06-08 11:10:42 -0400

    HTTPS and WSS server

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Niflheim
+
+Make ssl certificates:
+
+
+
+# Generate private key
+openssl genrsa --out test.key 1024
+
+# Or generate private key with passphrase
+openssl genrsa -des3 -out test.key 1024
+
+# Passphrase can be removed later
+openssl rsa -in test.key -out test.key.raw
+
+# Generate certificate signing request
+openssl req -new -sha256 -key test.key -out test.csr
+
+# Sign CSR with its own private key
+openssl x509 -req -sha256 -days 365 -in test.csr -out test.crt -signkey test.key
---
+++ server.js
@@ -0,0 +1,113 @@
+process.title = 'test-server';
+
+var repl    = require('repl');
+var fs      = require('fs');
+var hapi    = require('hapi');
+var ws      = require('ws');
+var moment  = require('moment');
+
+//////////////
+// Settings //
+//////////////
+
+var options;
+
+try {
+  options = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+} catch(e) {
+  console.log('Warning: Unable to read config file "' + __dirname + '/config.json". (' + e + ')');
+  console.log('Attempting to use "' + __dirname + '/default_config.json"...');
+  
+  try {
+    options = JSON.parse(fs.readFileSync(__dirname + '/default_config.json'));
+  } catch(e) {
+    console.log('Error: Unable to read config file "' + __dirname + '/default-config.json". (' + e + ')');
+    process.exit(1);
+  }
+}
+
+// Logger - Prints to stdout with timestamp, unless silenced by option. Add log file support?
+var log = options.silent ? function(){} : function(message){console.log(moment.utc(Date.now()).format('YYMMDD/HHmmss.SSS') + ', ' + message)};
+
+/////////////////
+// HTTP Server //
+/////////////////
+
+var server = new hapi.Server();
+
+server.connection({
+  host: options.host,
+  port: options.port,
+  tls: options.tls ? {
+    key: fs.readFileSync(__dirname + '/' + options.key),
+    cert: fs.readFileSync(__dirname + '/' + options.cert)
+  } : false
+});
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: __dirname + '/http',
+      index: true
+    }
+  }
+});
+
+server.start(function () {
+  log('Server running at: ' + server.info.uri);
+});
+
+///////////////
+// WS server //
+///////////////
+
+var wsServer = new ws.Server({server: server.listener, path: '/'});
+
+wsServer.on('connection', function(connection) {
+  log('Received WebSocket');
+  
+  connection.on('message', function(message) {
+    log('Received message: ' + message);
+  });
+  
+  connection.on('close', function() {
+    log('Closed WebSocket');
+  });
+});
+
+/////////////
+// Logging //
+/////////////
+
+server.register({
+  register: require('good'),
+  options: {
+    opsInterval: 15000,
+    reporters: [{
+      reporter: require('good-console'),
+      events: options.silent ? {error: '*'} : {error: '*', log: '*', response: '*', ops: '*'}
+    }]
+  }
+}, function () {});
+
+//////////
+// REPL //
+//////////
+
+if(options.repl) {
+  var cli = repl.start({});
+  
+  cli.context.repl               = repl;
+  cli.context.fs                 = fs;
+  cli.context.hapi               = hapi;
+  cli.context.ws                 = ws;
+  cli.context.moment             = moment;
+  
+  cli.context.options            = options;
+  cli.context.log                = log;
+  cli.context.server             = server;
+  cli.context.wsServer           = wsServer;
+  cli.context.cli                = cli;
+}
